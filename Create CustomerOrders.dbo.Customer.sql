2b145f915e443831632f62144d6a862651813fdb
VMBurn <83729217+VMBurn@users.noreply.github.com>
2021-05-07 21:24:27 +0200

    Add files via upload

---
+++ Create CustomerOrders.dbo.Customer.sql
@@ -0,0 +1,18 @@
+﻿USE [CustomerOrders]
+GO
+
+/****** Object: Table [dbo].[Customer] Script Date: 5/7/2021 9:19:14 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Customer] (
+    [CustomerId]  INT           NOT NULL,
+    [Name]        NVARCHAR (50) NOT NULL,
+    [DateOfBirth] DATE          NOT NULL,
+    [Country]     NVARCHAR (50) NOT NULL
+);
+
+
---
+++ Create CustomerOrders.dbo.Order.sql
@@ -0,0 +1,18 @@
+﻿USE [CustomerOrders]
+GO
+
+/****** Object: Table [dbo].[Order] Script Date: 5/7/2021 9:19:22 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Order] (
+    [OrderId]    INT        NOT NULL,
+    [CustomerId] INT        NOT NULL,
+    [Amount]     FLOAT (53) NOT NULL,
+    [VAT]        FLOAT (53) NOT NULL
+);
+
+
